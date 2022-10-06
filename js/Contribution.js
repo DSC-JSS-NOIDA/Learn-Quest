91c1c009a437f7dc601c1ee319174bb4ee50fe4d
Manan Agrawal <agrawalmanan2@gmail.com>
2022-10-07 00:00:47 +0530

    fixed nightmode on resources page

--- js/Contribution.js
+++ js/Contribution.js
@@ -1,34 +1,51 @@
 let darkmode = "false";
-let darkmodeicon = document.querySelector(".darkmodeicon");
+let flag = 0;
+$("#darkSwitch3").addClass("invisible");
+
+// Dark Mode Is Functional On The Resources Page
+
 function dark_mode() {
-    if (darkmode === "false") {
-        document.body.style.backgroundImage = "url('/assets/images/darkmode.png')";
-        document.body.style.color = "white";
-        darkmodeicon.classList.remove("fa-moon");
-        darkmodeicon.classList.add("fa-sun");
-        darkmode = "true";
-    }
-    else {
-        document.body.style.backgroundImage = "url('/assets/images/qlgb.png')";
-        document.body.style.color = "black";
-        darkmodeicon.classList.remove("fa-sun");
-        darkmodeicon.classList.add("fa-moon");
-        darkmode = "false";
-    }
+  if (flag == 0) {
+    $("#darkSwitch2").addClass("invisible");
+    $("#darkSwitch3").removeClass("invisible");
+    $("#darkSwitch3").addClass("visible");
+    $(".link-dark").addClass("btn-style-dark");
+    $(".link-dark").removeClass("btn-style");
+    
+    flag = 1;
+  } else if (flag == 1) {
+    $("#darkSwitch3").addClass("invisible");
+    $("#darkSwitch2").removeClass("invisible");
+    $("#darkSwitch2").addClass("visible");
+    $(".link-dark").removeClass("btn-style-dark");
+    $(".link-dark").addClass("btn-style");
+    flag = 0;
+  }
+  if (darkmode === "false") {
+    document.body.style.backgroundImage = "url('./assets/images/darkmode.png')";
+    document.body.style.color = "white";
+
+    darkmode = "true";
+  } else if (darkmode === "true") {
+    document.body.style.backgroundImage = "url('./assets/images/qlgb.png')";
+    document.body.style.color = "black";
+
+    darkmode = "false";
+  }
 }
 
 const toTop = document.querySelector(".to-top");
 const topBtn = document.querySelector(".btn-top");
 
-document.body.addEventListener("scroll",scrollTop);
-function scrollTop(){
-    if(document.body.scrollTop > 20){
-        toTop.style.display = "block";
-    }else{
-        toTop.style.display = "none";
-    }
+document.body.addEventListener("scroll", scrollTop);
+function scrollTop() {
+  if (document.body.scrollTop > 20) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
 }
 
-topBtn.addEventListener("click",() => {
-    document.body.scrollTop = 0;
-})
+topBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+});
